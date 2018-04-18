8a167ab6c09a73d22a682be32c98e3c7da7f5cda
AndyBrizhak <andrewbrizhak@gmail.com>
2018-04-18 20:59:09 +0300

    ++ add degree simple + degree recurse

--- DegreeUpNumber/DegreeUpNumber.c
+++ DegreeUpNumber/DegreeUpNumber.c
@@ -8,31 +8,51 @@
 //c. *Рекурсивно, используя свойство чётности степени.
 
 
-long power(int a, int b)
+int withoutrecursepower(int a, int b)
+//a.Без рекурсии.
 {
-	long p = 1;
+	int p = 1;
 	while (b)
 	{
 		p = p * a;
 		b--;
 	}
+	return p;
 }
 
+int simplerecursepower(int a, int b)
+//b.Рекурсивно.
+{
+	int p = 1;
+	if (b == 0) return 1;
+	else if (b<0)
+	{
+		return simplerecursepower(1.0 / a, -b);
+	}
+	else
+	{
+		return a * simplerecursepower(a, b - 1);
+	}
+	return p;
+}
 
 
 int main(int argc, char * argv[])
 {
 	char *locale = setlocale(LC_ALL, "");
 
-	int a, b;
-	long res;
-	printf("Введите целое десятичное число для возведения в степень: ");
-	scanf("%d\n", &a );
-	printf("Введите значение степени(целое число: ");
-	scanf("%d\n", &b);
-	res = power(a, b);
-	printf("Резульат возведения в степень: %l\n", res);
+	int a, b, res;
+	printf("Введите целое десятичное число для возведения в степень: \n");
+	scanf("%d", &a);
+	printf("Введите значение степени(целое число): \n");
+	scanf("%d", &b);
+	res = withoutrecursepower(a, b);
+	printf("Резульат возведения в степень(без рекурсии): %d\n", res);
 	getch();
+	res = simplerecursepower(a, b);
+	printf("Резульат возведения в степень(простая  рекурсия): %d\n", res);
+	getch();
+	//
 
 	return 0;
 }
